bad2ee9cc078e5c98d71f349b33bb5e00e6cf562
Drew <dvm0003@auburn.edu>
2017-01-05 20:39:03 -0600

    chat in toolbar

--- app/src/main/java/biome/fresco/MainActivity.java
+++ app/src/main/java/biome/fresco/MainActivity.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
@@ -67,15 +69,29 @@ public class MainActivity extends AppCompatActivity {
 
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
-
-
-
         toolbar = (Toolbar)findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
 
+    }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.messages:
+                FragmentManager fm = getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.container, Feed.newInstance("","")).addToBackStack("").commit();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void handleSignInResult(GoogleSignInResult result){
